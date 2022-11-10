ce3a65233593e36caa04877c12e6bf8b186ee5b8
Heinrich <heinrich@mohara.co>
2022-11-10 14:16:10 -0600

    Delete unneeded old weapon stuff and create the WeaponDataSO

---
+++ Assets/_Scripts/ScriptableObjects/WeaponDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Bardent._Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewWeaponData", menuName = "Data/Weapons/Base Weapon Data", order = 0)]
+    public class WeaponDataSO : ScriptableObject
+    {
+        
+    }
+}
