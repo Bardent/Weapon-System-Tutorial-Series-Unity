48cd0f6ae35412825466a046c4d2120f389a2f3f
Heinrich <heinrich@mohara.co>
2022-12-01 20:44:29 -0600

    Create attack movement data script and movement data

---
+++ Assets/_Scripts/Weapons/Components/ComponentData/AttackData/AttackMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Bardent.Weapons.Components.ComponentData.AttackData
+{
+    public class AttackMovement
+    {
+        [field: SerializeField] public Vector2 Direction { get; private set; }
+        [field: SerializeField] public float Velocity { get; private set; }
+    }
+}
