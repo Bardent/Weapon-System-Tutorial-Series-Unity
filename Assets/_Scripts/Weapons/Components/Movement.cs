ffbc29f1393d0952f2a5d7c8c4bd7ecbdefc1b9d
Heinrich <heinrich@mohara.co>
2022-12-01 20:38:34 -0600

    Create Handler functions and subscribe to them

--- Assets/_Scripts/Weapons/Components/Movement.cs
+++ Assets/_Scripts/Weapons/Components/Movement.cs
@@ -4,6 +4,30 @@ namespace Bardent.Weapons.Components
 {
     public class Movement : WeaponComponent
     {
-        
+        private void HandleStartMovement()
+        {
+            
+        }
+
+        private void HandleStopMovement()
+        {
+            
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            EventHandler.OnStartMovement += HandleStartMovement;
+            EventHandler.OnStopMovement += HandleStopMovement;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            
+            EventHandler.OnStartMovement -= HandleStartMovement;
+            EventHandler.OnStopMovement -= HandleStopMovement;
+        }
     }
 }
