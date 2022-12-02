2f98cccf67fe283685fde5d9c7cfccf926c971a3
Heinrich <heinrich@mohara.co>
2022-12-01 20:36:22 -0600

    Add start and stop movement events

--- Assets/_Scripts/Weapons/AnimationEventHandler.cs
+++ Assets/_Scripts/Weapons/AnimationEventHandler.cs
@@ -6,7 +6,12 @@ namespace Bardent.Weapons
     public class AnimationEventHandler : MonoBehaviour
     {
         public event Action OnFinish;
-        
+        public event Action OnStartMovement;
+        public event Action OnStopMovement;
+
+
         private void AnimationFinishedTrigger() => OnFinish?.Invoke();
+        private void StartMovementTrigger() => OnStartMovement?.Invoke();
+        private void StopMovementTrigger() => OnStopMovement?.Invoke();
     }
 }
