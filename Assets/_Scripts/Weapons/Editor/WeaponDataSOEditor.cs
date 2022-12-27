d35dd93e0c9d57b5e10858f573f48a279dccf623
Heinrich <heinrich@mohara.co>
2022-12-27 10:56:43 -0600

    Create the editor script for WeaponDataSO

---
+++ Assets/_Scripts/Weapons/Editor/WeaponDataSOEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Bardent.Weapons
+{
+    [CustomEditor(typeof(WeaponDataSO))]
+    public class WeaponDataSOEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
