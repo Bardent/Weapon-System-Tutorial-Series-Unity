de7f14061205c5578544edd11b5c543af4047d84
Heinrich <heinrich@mohara.co>
2022-12-30 21:27:46 -0600

    Part 9 one

---
+++ Assets/_Scripts/Weapons/Editor/WeaponDataSOEditor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bardent.Weapons.Components;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Bardent.Weapons
+{
+    [CustomEditor(typeof(WeaponDataSO))]
+    public class WeaponDataSOEditor : Editor
+    {
+        private static List<Type> dataCompTypes = new List<Type>();
+
+        private WeaponDataSO dataSO;
+
+        private void OnEnable()
+        {
+            dataSO = target as WeaponDataSO;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            foreach (var dataCompType in dataCompTypes)
+            {
+                if (GUILayout.Button(dataCompType.Name))
+                {
+                    var comp = Activator.CreateInstance(dataCompType) as ComponentData;
+                    
+                    if(comp == null)
+                        return;
+
+                    dataSO.AddData(comp);
+                }
+            }
+        }
+
+        [DidReloadScripts]
+        private static void OnRecompile()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var types = assemblies.SelectMany(assembly => assembly.GetTypes());
+            var filteredTypes = types.Where(
+                type => type.IsSubclassOf(typeof(ComponentData)) && !type.ContainsGenericParameters && type.IsClass
+            );
+            dataCompTypes = filteredTypes.ToList();
+        }
+    }
+}
