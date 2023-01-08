531c1036b9daf0b78c6853af52f09af4fabc9427
Heinrich <heinrich@mohara.co>
2023-01-07 21:55:36 -0600

    Create basic handler

--- Assets/_Scripts/Weapons/Components/Damage.cs
+++ Assets/_Scripts/Weapons/Components/Damage.cs
@@ -1,7 +1,15 @@
-﻿namespace Bardent.Weapons.Components
+﻿using UnityEngine;
+
+namespace Bardent.Weapons.Components
 {
     public class Damage : WeaponComponent<DamageData, AttackDamage>
     {
-        
+        private void HandleDetectCollider2D(Collider2D[] colliders)
+        {
+            foreach (var item in colliders)
+            {
+                print($"Detected Item: {item.name}");
+            }
+        }
     }
 }
