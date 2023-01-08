1bb72d9fd179fbe4b7cb4288ee186de36bf5d10f
Heinrich <heinrich@mohara.co>
2023-01-07 22:48:20 -0600

    Remove Damage code from Combat and move to DamageReceiver

--- Assets/_Scripts/Core/CoreComponents/Combat.cs
+++ Assets/_Scripts/Core/CoreComponents/Combat.cs
@@ -2,53 +2,50 @@
 
 namespace Bardent.CoreSystem
 {
-	public class Combat : CoreComponent, IDamageable, IKnockbackable
-	{
+    public class Combat : CoreComponent, IKnockbackable
+    {
+        private Movement Movement
+        {
+            get => movement ?? core.GetCoreComponent(ref movement);
+        }
 
-		[SerializeField] private GameObject damageParticles;
-	
-		private Movement Movement { get => movement ?? core.GetCoreComponent(ref movement); }
-		private CollisionSenses CollisionSenses {
-			get => collisionSenses ?? core.GetCoreComponent(ref collisionSenses);
-		}
-		private Stats Stats { get => stats ?? core.GetCoreComponent(ref stats); }
-		private ParticleManager ParticleManager => particleManager ? particleManager : core.GetCoreComponent(ref particleManager);
-	
-		private Movement movement;
-		private CollisionSenses collisionSenses;
-		private Stats stats;
-		private ParticleManager particleManager;
+        private CollisionSenses CollisionSenses
+        {
+            get => collisionSenses ?? core.GetCoreComponent(ref collisionSenses);
+        }
 
-		[SerializeField] private float maxKnockbackTime = 0.2f;
+        private Movement movement;
+        private CollisionSenses collisionSenses;
 
-		private bool isKnockbackActive;
-		private float knockbackStartTime;
 
-		public override void LogicUpdate() {
-			CheckKnockback();
-		}
+        [SerializeField] private float maxKnockbackTime = 0.2f;
 
-		public void Damage(float amount) {
-			Debug.Log(core.transform.parent.name + " Damaged!");
-			Stats?.DecreaseHealth(amount);
-			ParticleManager?.StartParticlesWithRandomRotation(damageParticles);
-		}
+        private bool isKnockbackActive;
+        private float knockbackStartTime;
 
-		public void Knockback(Vector2 angle, float strength, int direction) {
-			Movement?.SetVelocity(strength, angle, direction);
-			Movement.CanSetVelocity = false;
-			isKnockbackActive = true;
-			knockbackStartTime = Time.time;
-		}
+        public override void LogicUpdate()
+        {
+            CheckKnockback();
+        }
 
-		private void CheckKnockback() {
-			if (isKnockbackActive
-			    && ((Movement?.CurrentVelocity.y <= 0.01f && CollisionSenses.Ground)
-			        || Time.time >= knockbackStartTime + maxKnockbackTime)
-			   ) {
-				isKnockbackActive = false;
-				Movement.CanSetVelocity = true;
-			}
-		}
-	}
+        public void Knockback(Vector2 angle, float strength, int direction)
+        {
+            Movement?.SetVelocity(strength, angle, direction);
+            Movement.CanSetVelocity = false;
+            isKnockbackActive = true;
+            knockbackStartTime = Time.time;
+        }
+
+        private void CheckKnockback()
+        {
+            if (isKnockbackActive
+                && ((Movement?.CurrentVelocity.y <= 0.01f && CollisionSenses.Ground)
+                    || Time.time >= knockbackStartTime + maxKnockbackTime)
+               )
+            {
+                isKnockbackActive = false;
+                Movement.CanSetVelocity = true;
+            }
+        }
+    }
 }
