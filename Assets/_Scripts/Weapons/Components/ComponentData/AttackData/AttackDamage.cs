e8bf6d8e6b8b52c1f9ce557d52db972a6306aa7f
Heinrich <heinrich@mohara.co>
2023-01-07 21:52:43 -0600

    Create AttackDamage

---
+++ Assets/_Scripts/Weapons/Components/ComponentData/AttackData/AttackDamage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bardent.Weapons.Components
+{
+    [Serializable]
+    public class AttackDamage
+    {
+        [field: SerializeField] private float amount;
+    }
+}
