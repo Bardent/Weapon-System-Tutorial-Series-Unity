af48db46e467754536d10a630cc837750e25580a
Heinrich <heinrich@mohara.co>
2023-01-24 18:33:41 -0600

    Create the three main lists

--- Assets/_Scripts/Weapons/WeaponGenerator.cs
+++ Assets/_Scripts/Weapons/WeaponGenerator.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Bardent.Weapons.Components;
+using UnityEngine;
 
 namespace Bardent.Weapons
 {
     public class WeaponGenerator : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
+
+        private List<WeaponComponent> componentsAlreadyOnWeapon = new List<WeaponComponent>();
+        private List<WeaponComponent> componentsAddedToWeapon = new List<WeaponComponent>();
+        private List<Type> componentDependencies = new List<Type>();
 
         public void GenerateWeapon(WeaponDataSO data)
         {
