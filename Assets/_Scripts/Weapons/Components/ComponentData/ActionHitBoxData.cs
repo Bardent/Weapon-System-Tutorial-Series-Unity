8f0bf0002aa5e8874315a18ec7f91fc1b2a0d6af
Heinrich <heinrich@mohara.co>
2023-01-24 18:23:54 -0600

    Set ComponentDependency for all the component datas we have made so far

--- Assets/_Scripts/Weapons/Components/ComponentData/ActionHitBoxData.cs
+++ Assets/_Scripts/Weapons/Components/ComponentData/ActionHitBoxData.cs
@@ -5,5 +5,11 @@ namespace Bardent.Weapons.Components
     public class ActionHitBoxData : ComponentData<AttackActionHitBox>
     {
         [field: SerializeField] public  LayerMask DetectableLayers { get; private set; }
+        
+        public ActionHitBoxData()
+        {
+            ComponentDependency = typeof(ActionHitBox);
+        }
+
     }
 }
