47238da0fc2d5890b082e35b70044cd65af51202
Heinrich <heinrich@mohara.co>
2023-01-24 18:31:12 -0600

    Reference weapon and create function that takes in weapon data

--- Assets/_Scripts/Weapons/WeaponGenerator.cs
+++ Assets/_Scripts/Weapons/WeaponGenerator.cs
@@ -4,6 +4,11 @@ namespace Bardent.Weapons
 {
     public class WeaponGenerator : MonoBehaviour
     {
-        
+        [SerializeField] private Weapon weapon;
+
+        public void GenerateWeapon(WeaponDataSO data)
+        {
+            
+        }
     }
 }
