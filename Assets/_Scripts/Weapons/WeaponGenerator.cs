9fe50fc048779c48a43508121278b32acb96ad1b
Heinrich <heinrich@mohara.co>
2023-01-24 18:28:52 -0600

    Get reference to weapon and exit if there is no data

--- Assets/_Scripts/Weapons/WeaponGenerator.cs
+++ Assets/_Scripts/Weapons/WeaponGenerator.cs
@@ -2,8 +2,16 @@
 
 namespace Bardent.Weapons
 {
+    [RequireComponent(typeof(Weapon))]
     public class WeaponGenerator : MonoBehaviour
     {
+        [SerializeField] private Weapon weapon;
+
+        public void GenerateWeapon()
+        {
+            if (weapon.Data == null)
+                return;
+        }
         
     }
 }
