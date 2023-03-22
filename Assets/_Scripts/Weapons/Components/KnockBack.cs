1ded7626e56e379e798911ad87da9a27e483de70
Heinrich <heinrich@mohara.co>
2023-03-21 20:05:58 -0500

    Create handler and subscribe to events

--- Assets/_Scripts/Weapons/Components/KnockBack.cs
+++ Assets/_Scripts/Weapons/Components/KnockBack.cs
@@ -1,7 +1,30 @@
-﻿namespace Bardent.Weapons.Components
+﻿using UnityEngine;
+
+namespace Bardent.Weapons.Components
 {
     public class KnockBack : WeaponComponent<KnockBackData, AttackKnockBack>
     {
+        private ActionHitBox hitBox;
+
+        private void HandleDetectCollider2D(Collider2D[] colliders)
+        {
+            
+        }
         
+        protected override void Start()
+        {
+            base.Start();
+
+            hitBox = GetComponent<ActionHitBox>();
+
+            hitBox.OnDetectedCollider2D += HandleDetectCollider2D;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            hitBox.OnDetectedCollider2D -= HandleDetectCollider2D;
+        }
     }
 }
