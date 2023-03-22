5ce2d20e2d3e2128c949673d39fb4b85e6af8709
Heinrich <heinrich@mohara.co>
2023-03-21 20:07:56 -0500

    Add KnockBack data

--- Assets/_Scripts/Weapons/Components/ComponentData/AttackData/AttackKnockBack.cs
+++ Assets/_Scripts/Weapons/Components/ComponentData/AttackData/AttackKnockBack.cs
@@ -1,7 +1,10 @@
-﻿namespace Bardent.Weapons.Components
+﻿using UnityEngine;
+
+namespace Bardent.Weapons.Components
 {
     public class AttackKnockBack : AttackData
     {
-        
+        [field: SerializeField] public Vector2 KnockBackAngle { get; private set; }
+        [field: SerializeField] public float KnockBackStrength { get; private set;}
     }
 }
