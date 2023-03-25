cf92d3edf853cb79290c258fa5aceb8209f6d6bf
Heinrich <heinrich@mohara.co>
2023-03-25 17:51:39 -0500

    Create component and component data

---
+++ Assets/_Scripts/Weapons/Components/ComponentData/PoiseDamageData.cs
@@ -0,0 +1,10 @@
+﻿namespace Bardent.Weapons.Components
+{
+    public class PoiseDamageData : ComponentData<AttackPoiseDamage>
+    {
+        protected override void SetComponentDependency()
+        {
+            ComponentDependency = typeof(PoiseDamage);
+        }
+    }
+}
