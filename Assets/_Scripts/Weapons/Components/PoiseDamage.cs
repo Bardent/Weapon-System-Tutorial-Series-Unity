63e17894f3f2b8e791a238108ca28e4d6f0a7183
Heinrich <heinrich@mohara.co>
2023-03-25 17:54:07 -0500

    Reference hitbox and register handler

--- Assets/_Scripts/Weapons/Components/PoiseDamage.cs
+++ Assets/_Scripts/Weapons/Components/PoiseDamage.cs
@@ -1,7 +1,30 @@
-﻿namespace Bardent.Weapons.Components
+﻿using UnityEngine;
+
+namespace Bardent.Weapons.Components
 {
-    public class PoiseDamage
+    public class PoiseDamage : WeaponComponent<PoiseDamageData, AttackPoiseDamage>
     {
+        private ActionHitBox hitBox;
+
+        private void HandleDetectCollider2D(Collider2D[] colliders)
+        {
+            
+        }
         
+        protected override void Start()
+        {
+            base.Start();
+
+            hitBox = GetComponent<ActionHitBox>();
+
+            hitBox.OnDetectedCollider2D += HandleDetectCollider2D;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            hitBox.OnDetectedCollider2D -= HandleDetectCollider2D;
+        }
     }
 }
