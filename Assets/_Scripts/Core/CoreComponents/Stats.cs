13e19526513b71d389f1675b2498488748664199
Heinrich <heinrich@mohara.co>
2023-03-27 17:36:32 -0500

    Add Poise Recovery

--- Assets/_Scripts/Core/CoreComponents/Stats.cs
+++ Assets/_Scripts/Core/CoreComponents/Stats.cs
@@ -8,12 +8,21 @@ namespace Bardent.CoreSystem
     {
         [field: SerializeField] public Stat Health { get; private set; }
         [field: SerializeField] public Stat Poise { get; private set; }
+        [SerializeField] private float poiseRecovery;
         protected override void Awake()
         {
             base.Awake();
             
             Health.Init();
             Poise.Init();
+        }
+
+        private void Update()
+        {
+            if (Poise.CurrentValue < Poise.MaxValue)
+            {
+                Poise.Increase(poiseRecovery * Time.deltaTime);
+            }
         }
     }
 }
