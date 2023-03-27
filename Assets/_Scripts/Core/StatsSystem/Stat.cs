993ece677f3f25d480607f76934af9b2ecc26063
Heinrich <heinrich@mohara.co>
2023-03-27 17:12:45 -0500

    Create the stat class

---
+++ Assets/_Scripts/Core/StatsSystem/Stat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bardent.CoreSystem.StatsSystem
+{
+    [Serializable]
+    public class Stat
+    {
+        [field: SerializeField] public float MaxValue { get; private set; }
+
+        public event Action OnCurrentValueZero;
+
+        public float CurrentValue
+        {
+            get => currentValue;
+            private set
+            {
+                currentValue = Mathf.Clamp(value, 0f, MaxValue);
+
+                if (currentValue <= 0f)
+                {
+                    OnCurrentValueZero?.Invoke();
+                }
+            }
+        } 
+
+        private float currentValue;
+
+        public void Init() => CurrentValue = MaxValue;
+
+        public void Increase(float amount) => CurrentValue += amount;
+
+        public void Decrease(float amount) => CurrentValue -= amount;
+    }
+}
