012484f5a3a5d2fba3ca1909cb479b769a46765f
Heinrich <heinrich@mohara.co>
2023-03-27 17:13:38 -0500

    Remove old stat stuff

--- Assets/_Scripts/Core/CoreComponents/Stats.cs
+++ Assets/_Scripts/Core/CoreComponents/Stats.cs
@@ -5,35 +5,9 @@ namespace Bardent.CoreSystem
 {
     public class Stats : CoreComponent
     {
-        public event Action OnHealthZero;
-    
-        [SerializeField] private float maxHealth;
-        private float currentHealth;
-
         protected override void Awake()
         {
             base.Awake();
-
-            currentHealth = maxHealth;
-        }
-
-        public void DecreaseHealth(float amount)
-        {
-            currentHealth -= amount;
-
-            if(currentHealth <= 0)
-            {
-                currentHealth = 0;
-            
-                OnHealthZero?.Invoke();
-            
-                Debug.Log("Health is zero!!");
-            }
-        }
-
-        public void IncreaseHealth(float amount)
-        {
-            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         }
     }
 }
