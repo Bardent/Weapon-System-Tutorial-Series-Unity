c24acb5eaaeaee0a6a423fa0956c771dd4211fbd
Heinrich <heinrich@mohara.co>
2023-04-01 10:08:16 -0500

    Create the weapon phase animation event

--- Assets/_Scripts/Weapons/AnimationEventHandler.cs
+++ Assets/_Scripts/Weapons/AnimationEventHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using Bardent.Weapons.Enums;
 using UnityEngine;
 
 namespace Bardent.Weapons
@@ -9,10 +10,13 @@ namespace Bardent.Weapons
         public event Action OnStartMovement;
         public event Action OnStopMovement;
         public event Action OnAttackAction;
-        
+
+        public event Action<AttackPhases> OnEnterAttackPhase; 
+
         private void AnimationFinishedTrigger() => OnFinish?.Invoke();
         private void StartMovementTrigger() => OnStartMovement?.Invoke();
         private void StopMovementTrigger() => OnStopMovement?.Invoke();
         private void AttackActionTrigger() => OnAttackAction?.Invoke();
+        private void EnterAttackPhase(AttackPhases phase) => OnEnterAttackPhase?.Invoke(phase);
     }
 }
