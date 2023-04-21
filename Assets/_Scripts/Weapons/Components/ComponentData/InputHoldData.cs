fd2c1f67cf8c4d699d6514bf248a2142afd59cfe
Heinrich <heinrich@mohara.co>
2023-04-21 16:58:50 -0500

    Part 17 Done

---
+++ Assets/_Scripts/Weapons/Components/ComponentData/InputHoldData.cs
@@ -0,0 +1,10 @@
+﻿namespace Bardent.Weapons.Components
+{
+    public class InputHoldData : ComponentData
+    {
+        protected override void SetComponentDependency()
+        {
+            ComponentDependency = typeof(InputHold);
+        }
+    }
+}
