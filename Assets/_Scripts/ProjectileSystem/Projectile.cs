1c6ffdcebe524a05f0f39ab60f8826e655a08a52
Heinrich <heinrich@mohara.co>
2023-04-28 16:33:29 -0500

    Create the Projectile Monobehavior and GameObject

---
+++ Assets/_Scripts/ProjectileSystem/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Bardent.ProjectileSystem
+{
+    /// <summary>
+    /// This class is the interface between projectile components and any entity that spawns a projectile.
+    /// </summary>
+    public class Projectile : MonoBehaviour
+    {
+        
+    }
+}
