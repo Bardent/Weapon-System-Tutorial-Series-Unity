1dc902b313bb6644dd03d3fabb4e3a10cfad11d2
Heinrich <heinrich@mohara.co>
2023-04-28 16:42:07 -0500

    Create the base projectile component class

---
+++ Assets/_Scripts/ProjectileSystem/Components/ProjectileComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bardent.ProjectileSystem.Components
+{
+    public class ProjectileComponent : MonoBehaviour
+    {
+        protected Projectile projectile;
+
+        #region Plumbing
+
+        private void Awake()
+        {
+            projectile = GetComponent<Projectile>();
+        }
+
+        #endregion
+    }
+}
