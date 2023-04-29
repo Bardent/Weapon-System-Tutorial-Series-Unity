8ccf027656d9c0500f9bebed57b406522ec8180e
Heinrich <heinrich@mohara.co>
2023-04-28 20:26:08 -0500

    Add projectile HitBox and StickToLayer components + some utilities

---
+++ Assets/_Scripts/ProjectileSystem/Components/HitBox.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Bardent.Utilities;
+using UnityEngine;
+
+namespace Bardent.ProjectileSystem.Components
+{
+    /// <summary>
+    /// This class is a generic HitBox used by projectiles. The HitBox shape itself is defined by a Rect and it uses BoxCastAll to
+    /// do the physics check. When things are detected, it fires off an event with all the RaycastHit2D information for other components to use
+    /// </summary>
+    public class HitBox : ProjectileComponent
+    {
+        public event Action<RaycastHit2D[]> OnRaycastHit2D;
+
+        [field: SerializeField] public Rect HitBoxRect { get; private set; }
+        [field: SerializeField] public LayerMask LayerMask { get; private set; }
+
+        private RaycastHit2D[] hits;
+        private float checkDistance;
+
+        private Transform _transform;
+
+        private void CheckHitBox()
+        {
+            hits = Physics2D.BoxCastAll(transform.TransformPoint(HitBoxRect.center), HitBoxRect.size,
+                _transform.rotation.eulerAngles.z, _transform.right, checkDistance, LayerMask);
+
+            if (hits.Length <= 0) return;
+
+            OnRaycastHit2D?.Invoke(hits);
+        }
+
+        #region Plumbing
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            // Just caching the transform based on repeated use (Recommendation from Rider IDE)
+            _transform = transform;
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            // Used to compensate for projectile velocity to help stop clipping
+            checkDistance = rb.velocity.magnitude * Time.deltaTime;
+
+            CheckHitBox();
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // The following is some code that ChatGPT Generated for me to visualize the HitBoxRect based on the rotation.
+            // Set up gizmo color
+            Gizmos.color = Color.red;
+
+            // Create a new matrix that applies the projectile's rotation
+            Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position,
+                Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z), Vector3.one);
+            Gizmos.matrix = rotationMatrix;
+
+            // Draw the wireframe cube
+            Gizmos.DrawWireCube(HitBoxRect.center, HitBoxRect.size);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/_Scripts/ProjectileSystem/Components/StickToLayer.cs
@@ -0,0 +1,86 @@
+﻿using Bardent.Utilities;
+using UnityEngine;
+
+namespace Bardent.ProjectileSystem.Components
+{
+    /// <summary>
+    /// This component is responsible for ensuring the projectile gets stuck in a specific layer based on what the HitBox detects
+    /// </summary>
+    [RequireComponent(typeof(HitBox))]
+    public class StickToLayer : ProjectileComponent
+    {
+        [field: SerializeField] public LayerMask LayerMask { get; private set; }
+        [field: SerializeField] public float CheckDistance { get; private set; }
+
+        private bool isStuck;
+
+        private HitBox hitBox;
+
+        private void HandleRaycastHit2D(RaycastHit2D[] hits)
+        {
+            if (isStuck)
+                return;
+
+            // The point returned by the boxcast can be weird, so we do one last check by firing a ray from the origin to the right to find
+            // a more suitable resting point
+            var lineHit = Physics2D.Raycast(transform.position, transform.right, CheckDistance, LayerMask);
+
+            // Remove velocity and set body to static so that it is not affected by other things. This might have to be adjusted
+            // depending on the behavior you want
+            rb.velocity = Vector2.zero;
+            rb.bodyType = RigidbodyType2D.Static;
+
+            isStuck = true;
+
+            //If out line hit finds a collider to use, then use it.
+            if (lineHit)
+            {
+                transform.position = lineHit.point;
+                return;
+            }
+
+            // Otherwise look through the hits from the HitBox
+            foreach (var hit in hits)
+            {
+                //HitBox might detect things on more layers than we care about so,
+                //did this hit happen with the correct LayerMask we are interested in?
+                if (!LayerMaskUtilities.IsLayerInMask(hit, LayerMask))
+                    continue;
+
+                transform.position = hit.point;
+                return;
+            }
+
+            // If there is nothing to get stuck in, set isStuck to false and make body dynamic again so it will fall
+            isStuck = false;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+
+            isStuck = false;
+        }
+
+        #region Plumbing
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            hitBox = GetComponent<HitBox>();
+
+            hitBox.OnRaycastHit2D += HandleRaycastHit2D;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            hitBox.OnRaycastHit2D -= HandleRaycastHit2D;
+        }
+
+        #endregion
+    }
+}
