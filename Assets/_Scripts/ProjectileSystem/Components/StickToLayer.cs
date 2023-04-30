b43663cce05e5605f99cfe7d724b8caf5e879bfa
Heinrich <heinrich@mohara.co>
2023-04-29 23:13:19 -0500

    Update StickToLayer component to account for object it is attached to being disabled or destroyed

--- Assets/_Scripts/ProjectileSystem/Components/StickToLayer.cs
+++ Assets/_Scripts/ProjectileSystem/Components/StickToLayer.cs
@@ -1,4 +1,6 @@
-﻿using Bardent.Utilities;
+﻿using System;
+using System.Collections;
+using Bardent.Utilities;
 using UnityEngine;
 
 namespace Bardent.ProjectileSystem.Components
@@ -10,10 +12,13 @@ namespace Bardent.ProjectileSystem.Components
     public class StickToLayer : ProjectileComponent
     {
         [field: SerializeField] public LayerMask LayerMask { get; private set; }
+
+        // SpriteRenderer sorting to be used when projectile is stuck
         [field: SerializeField] public string InactiveSortingLayerName { get; private set; }
         [field: SerializeField] public float CheckDistance { get; private set; }
 
         private bool isStuck;
+        private bool subscribedToDisableNotifier;
 
         private HitBox hitBox;
 
@@ -21,27 +26,30 @@ namespace Bardent.ProjectileSystem.Components
 
         private SpriteRenderer sr;
 
+        private OnDisableNotifier onDisableNotifier;
+
+        private Transform referenceTransform;
+        private Transform _transform;
+
+        private Vector3 offsetPosition;
+        private Quaternion offsetRotation;
+
+
         private void HandleRaycastHit2D(RaycastHit2D[] hits)
         {
             if (isStuck)
                 return;
 
+            SetStuck();
+
             // The point returned by the boxcast can be weird, so we do one last check by firing a ray from the origin to the right to find
             // a more suitable resting point
-            var lineHit = Physics2D.Raycast(transform.position, transform.right, CheckDistance, LayerMask);
-
-            // Remove velocity and set body to static so that it is not affected by other things. This might have to be adjusted
-            // depending on the behavior you want
-            sr.sortingLayerName = InactiveSortingLayerName;
-            rb.velocity = Vector2.zero;
-            rb.bodyType = RigidbodyType2D.Static;
-
-            isStuck = true;
+            var lineHit = Physics2D.Raycast(_transform.position, _transform.right, CheckDistance, LayerMask);
 
             //If out line hit finds a collider to use, then use it.
             if (lineHit)
             {
-                transform.position = lineHit.point;
+                SetReferenceTransformAndPoint(lineHit.transform, lineHit.point);
                 return;
             }
 
@@ -53,15 +61,63 @@ namespace Bardent.ProjectileSystem.Components
                 if (!LayerMaskUtilities.IsLayerInMask(hit, LayerMask))
                     continue;
 
-                transform.position = hit.point;
+                SetReferenceTransformAndPoint(hit.transform, hit.point);
                 return;
             }
 
             // If there is nothing to get stuck in, set isStuck to false and make body dynamic again so it will fall
-            sr.sortingLayerName = activeSortingLayerName;
+            SetUnstuck();
+        }
+
+        // Set projectile position to point and set new reference transform for projectile to track
+        private void SetReferenceTransformAndPoint(Transform newReferenceTransform, Vector2 newPoint)
+        {
+            if (newReferenceTransform.TryGetComponent(out onDisableNotifier))
+            {
+                onDisableNotifier.OnDisableEvent += HandleDisableNotifier;
+                subscribedToDisableNotifier = true;
+            }
+
+            // Set projectile position to detected point
+            _transform.position = newPoint;
+
+            // Set reference transform and cache position and rotation offset
+            referenceTransform = newReferenceTransform;
+            offsetPosition = _transform.position - referenceTransform.position;
+            offsetRotation = Quaternion.Inverse(referenceTransform.rotation) * _transform.rotation;
+        }
+
+        // Set Rigidbody2D bodyType to static so that it is not affected by gravity and set sorting layer such that projectile appears behind other items
+        private void SetStuck()
+        {
+            isStuck = true;
+
+            sr.sortingLayerName = InactiveSortingLayerName;
+            rb.velocity = Vector2.zero;
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+
+        // Set Rigidbody2D bodyType to dynamic so that it is affected by gravity again and set sorting layer such that projectile appears in front of other items
+        private void SetUnstuck()
+        {
             isStuck = false;
+
+            sr.sortingLayerName = activeSortingLayerName;
             rb.bodyType = RigidbodyType2D.Dynamic;
         }
+
+        // If the body we are stuck in gets disabled or destroyed, make projectile dynamic again
+        private void HandleDisableNotifier()
+        {
+            SetUnstuck();
+
+            if (!subscribedToDisableNotifier)
+                return;
+
+            onDisableNotifier.OnDisableEvent -= HandleDisableNotifier;
+            subscribedToDisableNotifier = false;
+        }
+
 
         protected override void Init()
         {
@@ -76,6 +132,8 @@ namespace Bardent.ProjectileSystem.Components
         {
             base.Awake();
 
+            _transform = transform;
+
             sr = GetComponentInChildren<SpriteRenderer>();
             activeSortingLayerName = sr.sortingLayerName;
 
@@ -84,11 +142,29 @@ namespace Bardent.ProjectileSystem.Components
             hitBox.OnRaycastHit2D += HandleRaycastHit2D;
         }
 
+        protected override void Update()
+        {
+            base.Update();
+
+            if (!isStuck)
+                return;
+
+            // Update position and rotation based on reference transform
+            var referenceRotation = referenceTransform.rotation;
+            _transform.position = referenceTransform.position + referenceRotation * offsetPosition;
+            _transform.rotation = referenceRotation * offsetRotation;
+        }
+
         protected override void OnDestroy()
         {
             base.OnDestroy();
 
             hitBox.OnRaycastHit2D -= HandleRaycastHit2D;
+
+            if (subscribedToDisableNotifier)
+            {
+                onDisableNotifier.OnDisableEvent -= HandleDisableNotifier;
+            }
         }
 
         #endregion
---
+++ Assets/_Scripts/ProjectileSystem/OnDisableNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bardent.ProjectileSystem
+{
+    /// <summary>
+    /// This class fires off an event whenever the GameObject it is attached to is disabled or destroyed
+    /// </summary>
+    public class OnDisableNotifier : MonoBehaviour
+    {
+        public event Action OnDisableEvent;
+
+        private void OnDisable()
+        {
+            OnDisableEvent?.Invoke();
+        }
+
+        [ContextMenu("Test")]
+        private void Test()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
