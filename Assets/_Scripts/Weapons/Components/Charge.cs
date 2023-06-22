3000a0d8985ae1876360e148f8e24584eb019f4b
Heinrich <heinrich@mohara.co>
2023-06-21 23:15:16 -0400

    Update Charge.cs

--- Assets/_Scripts/Weapons/Components/Charge.cs
+++ Assets/_Scripts/Weapons/Components/Charge.cs
@@ -7,17 +7,57 @@ namespace Bardent.Weapons.Components
         private int currentCharge;
 
         private TimeNotifier timeNotifier;
-        
+
         protected override void HandleEnter()
         {
             base.HandleEnter();
 
             currentCharge = currentAttackData.InitialChargeAmount;
+            
+            timeNotifier.Init(currentAttackData.ChargeTime, true);
+        }
+
+        private void HandleNotify()
+        {
+            currentCharge++;
+
+            if (currentCharge >= currentAttackData.NumberOfCharges)
+            {
+                currentCharge = currentAttackData.NumberOfCharges;
+                timeNotifier.Disable();
+                // Spawn fully charge particles
+                return;
+            }
+            else
+            {
+                //spawn single charge particle
+            }
+        }
+
+        protected override void HandleExit()
+        {
+            base.HandleExit();
+            
+            timeNotifier.Disable();
         }
 
         #region Plumbing
 
-        
+        protected override void Awake()
+        {
+            base.Awake();
+
+            timeNotifier = new TimeNotifier();
+
+            timeNotifier.OnNotify += HandleNotify;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            timeNotifier.OnNotify -= HandleNotify;
+        }
 
         #endregion
     }
