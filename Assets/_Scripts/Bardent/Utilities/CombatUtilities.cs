f007dd9f33202a3a186bc2a38b00e5e9714887fb
Heinrich <37575819+Bardent@users.noreply.github.com>
2023-07-19 21:56:40 -0400

    Update CombatUtilities.cs

--- Assets/_Scripts/Bardent/Utilities/CombatUtilities.cs
+++ Assets/_Scripts/Bardent/Utilities/CombatUtilities.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 namespace Bardent.Utilities
 {
+    /*
+     * This Utility class provides some static functions for logic we might perform in many different places. This way
+     * we can keep it all consolidated here and only have to change it in one place. That is the dream anyway.
+     *
+     * For example: The Damage functions are called by both DamageOnHitBoxAction and DamageOnBlock weapon components.
+     */
     public static class CombatUtilities
     {
         public static void Damage(GameObject gameObject, DamageData damageData)
         {
+            // TryGetComponentInChildren is a custom GameObject extension method.
             if (gameObject.TryGetComponentInChildren(out IDamageable damageable))
             {
                 damageable.Damage(damageData);
